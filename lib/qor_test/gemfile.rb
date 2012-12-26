a4a4254ac61db5c1399628f58b3aab1d891034ca
Jinzhu <wosmvp@gmail.com>
2012-12-26 11:48:25 +0800

    reduce method complexity

--- lib/qor_test/gemfile.rb
+++ lib/qor_test/gemfile.rb
@@ -35,17 +35,19 @@ module Qor
         end
       end
 
-      def write_gemfile(gems_from_config, gems_from_gemfile, filename)
+      def generate_gems_entry(gems_from_config, gems_from_gemfile)
         gem_names = [gems_from_config.keys, gems_from_gemfile.keys].flatten.compact.uniq
 
-        gems = gem_names.map do |name|
+        gem_names.map do |name|
           gems_from_config[name] || gems_from_gemfile[name]
-        end.compact
+        end.compact.map(&:to_s).join("\n")
+      end
 
+      def write_gemfile(gems_from_config, gems_from_gemfile, filename)
         File.open(filename, "w+") do |f|
           f << Qor::Test::Configuration.combined_sources(options)
           f << Qor::Test::Configuration.combined_gemspecs(options)
-          f << gems.map(&:to_s).join("\n")
+          f << generate_gems_entry(gems_from_config, gems_from_gemfile)
         end.path
       end
     end
