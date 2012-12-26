a1f57e61f90da96a22e497a890759ee5d5bc3d53
Jinzhu <wosmvp@gmail.com>
2012-12-26 15:23:50 +0800

    use qor_test to test qor_test

--- lib/qor_test/gem.rb
+++ lib/qor_test/gem.rb
@@ -1,29 +1,28 @@
 module Qor
   module Test
     class Gem
-      attr_accessor :name, :options, :gem_option
+      attr_accessor :name, :options
 
       def initialize(node, gem_option=nil)
         self.name = node.name.to_s
-        self.gem_option = gem_option
+        self.options = gem_option.is_a?(Array) ? gem_option : [gem_option]
 
         [:git, :path, :platforms].map do |type|
           if node.parent.is_node?(type)
-            self.options = {type => node.parent.value}.merge(node.parent.options)
+            parent_option = {type => node.parent.value}.merge(node.parent.options)
+            if options[-1].is_a?(Hash)
+              self.options[-1] = parent_option.merge(options[-1])
+            else
+              self.options.push(parent_option)
+            end
           end
         end
       end
 
       def to_s
-        if gem_option.is_a?(Array) && gem_option.length > 0
-          %{gem "#{name}", #{gem_option.map(&:inspect).join(", ")}}
-        elsif !gem_option.nil? && !gem_option.is_a?(Array)
-          %{gem "#{name}", #{gem_option.inspect}}
-        elsif options.is_a?(Hash)
-          %{gem "#{name}", #{options.inspect}}
-        else
-          %{gem "#{name}"}
-        end
+        result = %{gem "#{name}"}
+        result += %{, #{options.map(&:inspect).join(", ")}} if options.length > 0
+        result
       end
 
       def self.parse(node)
---
+++ test/gem_test.rb
@@ -0,0 +1,98 @@
+require 'minitest/autorun'
+require 'qor_test'
+
+describe Gem do
+  it "gem to string" do
+    # case 1
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      gem "rails", "3.1"
+    end
+
+    Qor::Test::Gem.parse(config.first("gem")).length.must_equal 1
+    Qor::Test::Gem.parse(config.first("gem"))[0].to_s.must_equal 'gem "rails", "3.1"'
+
+    # case 2
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      gem "rails", ["3.1", "3.2"]
+    end
+
+    Qor::Test::Gem.parse(config.first("gem")).length.must_equal 2
+    Qor::Test::Gem.parse(config.first("gem"))[0].to_s.must_equal 'gem "rails", "3.1"'
+    Qor::Test::Gem.parse(config.first("gem"))[1].to_s.must_equal 'gem "rails", "3.2"'
+
+    # case 3
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      gem 'nokogiri', [{:git => "git://github.com/tenderlove/nokogiri.git", :branch => "1.4"}, {:git => "git://github.com/tenderlove/nokogiri.git"}]
+    end
+
+    Qor::Test::Gem.parse(config.first("gem")).length.must_equal 2
+    Qor::Test::Gem.parse(config.first("gem"))[0].to_s.must_equal 'gem "nokogiri", {:git=>"git://github.com/tenderlove/nokogiri.git", :branch=>"1.4"}'
+    Qor::Test::Gem.parse(config.first("gem"))[1].to_s.must_equal 'gem "nokogiri", {:git=>"git://github.com/tenderlove/nokogiri.git"}'
+
+    # case 4
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      gem 'nokogiri', :git => "git://github.com/tenderlove/nokogiri.git", :branch => "1.4"
+    end
+
+    Qor::Test::Gem.parse(config.first("gem")).length.must_equal 1
+    Qor::Test::Gem.parse(config.first("gem"))[0].to_s.must_equal 'gem "nokogiri", {:git=>"git://github.com/tenderlove/nokogiri.git", :branch=>"1.4"}'
+
+    # case 5
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      gem 'RedCloth', ">= 4.1.0", "< 4.2.0"
+    end
+
+    Qor::Test::Gem.parse(config.first("gem")).length.must_equal 1
+    Qor::Test::Gem.parse(config.first("gem"))[0].to_s.must_equal 'gem "RedCloth", ">= 4.1.0", "< 4.2.0"'
+
+    # case 6
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      git "git://github.com/tenderlove/nokogiri.git" do
+        gem 'nokogiri', '1.4'
+      end
+    end
+
+    Qor::Test::Gem.parse(config.deep_find("gem")[0]).length.must_equal 1
+    Qor::Test::Gem.parse(config.deep_find("gem")[0])[0].to_s.must_equal 'gem "nokogiri", "1.4", {:git=>"git://github.com/tenderlove/nokogiri.git"}'
+
+    # case 7
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      git "git://github.com/tenderlove/nokogiri.git" do
+        gem 'nokogiri', :branch => '1.4'
+      end
+    end
+
+    Qor::Test::Gem.parse(config.deep_find("gem")[0]).length.must_equal 1
+    Qor::Test::Gem.parse(config.deep_find("gem")[0])[0].to_s.must_equal 'gem "nokogiri", {:git=>"git://github.com/tenderlove/nokogiri.git", :branch=>"1.4"}'
+
+    # case 8
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      gem "rails"
+    end
+
+    Qor::Test::Gem.parse(config.first("gem")).length.must_equal 1
+    Qor::Test::Gem.parse(config.first("gem"))[0].to_s.must_equal 'gem "rails"'
+
+    # case 9
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      git "git://github.com/tenderlove/nokogiri.git" do
+        gem 'nokogiri', ['1.4', '2.0']
+      end
+    end
+
+    Qor::Test::Gem.parse(config.deep_find("gem")[0]).length.must_equal 2
+    Qor::Test::Gem.parse(config.deep_find("gem")[0])[0].to_s.must_equal 'gem "nokogiri", "1.4", {:git=>"git://github.com/tenderlove/nokogiri.git"}'
+    Qor::Test::Gem.parse(config.deep_find("gem")[0])[1].to_s.must_equal 'gem "nokogiri", "2.0", {:git=>"git://github.com/tenderlove/nokogiri.git"}'
+
+    # case 10
+    config = Qor::Test::Configuration.load(nil, :force => true) do
+      git "git://github.com/tenderlove/nokogiri.git" do
+        gem 'nokogiri', [{:branch => '1.4'}, {:branch => '2.0'}]
+      end
+    end
+
+    Qor::Test::Gem.parse(config.deep_find("gem")[0]).length.must_equal 2
+    Qor::Test::Gem.parse(config.deep_find("gem")[0])[0].to_s.must_equal 'gem "nokogiri", {:git=>"git://github.com/tenderlove/nokogiri.git", :branch=>"1.4"}'
+    Qor::Test::Gem.parse(config.deep_find("gem")[0])[1].to_s.must_equal 'gem "nokogiri", {:git=>"git://github.com/tenderlove/nokogiri.git", :branch=>"2.0"}'
+  end
+end
